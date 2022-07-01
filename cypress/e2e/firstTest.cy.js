a43a12e1d03f543b766e2aa3d078388b8a774d1f
Artem Bondar <artem.bondar@gmail.com>
2022-06-30 19:31:47 -0500

    finding web elements

--- cypress/e2e/firstTest.cy.js
+++ cypress/e2e/firstTest.cy.js
@@ -10,7 +10,7 @@ describe('First test suite', () => {
         cy.get('nb-user').should('contain', 'Nick Jones')
     })
 
-    it.only('Types of locators', () => {
+    it('Types of locators', () => {
         cy.contains('Forms').click()
         cy.contains('Form Layouts').click()
 
@@ -42,10 +42,48 @@ describe('First test suite', () => {
         cy.get('[id*="Email1"]')
 
         //element with a value that starts with
-        cy.get('[id^=input]')
+        cy.get('[id^=input]').contains('email')
 
         //element witha a value ends with
         cy.get('[id$=Email1]')
+    })
+
+    it.only('finding web elements', () => {
+        cy.contains('Forms').click()
+        cy.contains('Form Layouts').click()
+
+        // get() - search elements globally by locator
+        // find() - seach child elements within the scope of previous DOM object
+        // contains() - seach elements by text.
+
+        cy.contains('Sign in')
+
+        cy.contains('[status="warning"]','Sign in')
+
+        cy.get('#inputEmail1')
+            .parents('form')
+            .find('button')
+            .should('contain', 'Sign in')
+            .parents('form')
+            .find('nb-radio')
+            .first()
+            .find('input')
+            .click({force: true})
+        
+        cy.get('.inline-form-card').find('button')
+        cy.get('.inline-form-card button')
+
+        cy.get('.inline-form-card').contains('Submit')
+
+        cy.contains('nb-card','Horizontal form').find('button')
+
+        //helper commands
+        // first() - find the first element
+        // last() -find the last element from the list
+        // next() - find the next element siblings
+        // prev() - previous element siblings
+        // eq() - find by index
+        // not() - exclude some element from the list
     })
 
 })
