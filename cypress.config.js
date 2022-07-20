5ff8ef834ef6f5e10e7fa3090b0f70e043fcd1f6
Artem Bondar <artem.bondar@gmail.com>
2022-07-19 19:37:01 -0500

    working with APIs

--- cypress.config.js
+++ cypress.config.js
@@ -4,7 +4,7 @@ module.exports = defineConfig({
   viewportHeight: 1080,
   viewportWidth: 1920,
   e2e: {
-    baseUrl: 'http://localhost:4200',
+    baseUrl: 'https://angular.realworld.io/',
     excludeSpecPattern: ['**/1-getting-started/*', '**/2-advanced-examples/*'],
     setupNodeEvents(on, config) {
       // implement node event listeners here
---
+++ cypress/e2e/workWithApi.cy.js
@@ -0,0 +1,54 @@
+
+
+describe('work with apis', () => {
+
+    it('work with tags', () => {
+        // cy.intercept('GET', 'https://api.realworld.io/api/tags', {fixture: 'tags.json'})
+        cy.intercept({method: 'Get', path: 'tags'}, {fixture: 'tags.json'})
+        cy.visit('/')
+    })
+
+    it('verify api response', () => {
+        cy.intercept('GET', 'https://api.realworld.io/api/tags').as('getTags')
+        cy.visit('/')
+        cy.wait('@getTags').then( myResponseTags => {
+            const secondTag = myResponseTags.response.body.tags[1]
+            expect(secondTag).to.equal('welcome')
+        })
+    })
+
+    it.only('create and delete article', () => {
+        cy.intercept('https://api.realworld.io/api/users/login').as('getToken')
+        cy.visit('/')
+        cy.contains('Sign in').click()
+        cy.get('[placeholder="Email"]').type('beauty123@test.com')
+        cy.get('[placeholder="Password"]').type('Welcome1')
+        cy.get('[type="submit"]').click()
+
+        cy.wait('@getToken').then( tokenResponse => {
+            const token = tokenResponse.response.body.user.token
+            cy.wrap(token).as('myToken')
+        })
+
+        cy.fixture('newArticle.json').then(articleRequestBody => {
+            cy.get('@myToken').then( myToken => {
+                cy.request({
+                    url: 'https://api.realworld.io/api/articles/',
+                    headers: {'Authorization': 'Token ' + myToken},
+                    method: 'POST',
+                    body: articleRequestBody
+                })
+            })
+        })
+
+        cy.contains('Global Feed').click()
+        cy.contains('Test title 2').click()
+        cy.contains('Delete Article').eq(0).click()
+        cy.intercept('https://api.realworld.io/api/articles?limit=10&offset=0').as('globalFeed')
+        cy.contains('Global Feed').click()
+        cy.wait('@globalFeed')
+        cy.get('app-article-list').should('not.contain', 'Test title 2')
+    })
+
+
+})
